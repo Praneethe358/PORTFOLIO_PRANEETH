ff86bb7db8fed49322ca2d287957275168a99ccd
praneethe358 <praneethpraneeth8888@gmail.com>
2026-04-17 21:04:51 +0530

    CHANGED THE ABOUT SECTION COMPLETELY

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -71,7 +71,7 @@ const Skills = () => {
           transition={{ duration: 0.6 }}
         >
           <h2 className="text-2xl md:text-3xl font-bold text-white mb-2 inline-block">
-            {'Skills'.split('').map((char, i) => (
+            {'Technical Proficiency'.split('').map((char, i) => (
               <motion.span
                 key={i}
                 initial={{ opacity: 0, y: 20 }}
